61a228d352b6e0df1156347a84fe67985ffe70a4
Ryan Endacott <rzeg24@gmail.com>
2014-02-15 21:27:49 -0600

    now gets flow points successfully

--- main.rb
+++ main.rb
@@ -1,16 +1,40 @@
 
 require 'yaml'
+require 'hue'
 require 'github_api'
+require 'active_support/all'
+
+MAX_HUE = 65535
+MAX_BRIGHT = 255
+MAX_SAT = 255
 
 # Set up configuration
 config = YAML.load_file('application.yml')
 
-github = Github.new do |config|
-  config.endpoint    = 'https://github.company.com/api/v3'
-  config.site        = 'https://github.company.com'
-  config.oauth_token = 'token'
-  config.adapter     = :net_http
-  config.ssl         = {:verify => false}
+github = Github.new login: config['GITHUB_USERNAME'], password: config['GITHUB_PASSWORD']
+
+recent_repos = github.repos.all.sort_by {|r| Date.parse(r.updated_at) }
+
+recent_repos.reverse!
+
+twentyfour_hours_ago = Time.new.utc.advance(hours: -24).iso8601
+
+flow_points = 0
+
+recent_repos[0..3].each do |repo|
+  commits = github.repos.commits.all(repo.owner.login, repo.name, since: twentyfour_hours_ago)
+  flow_points += commits.count
 end
+
+puts flow_points
+
+=begin
+hue = Hue::Client.new
+
+hue.lights.each do |light|
+  light.on = true
+  light.hue = Random.new.rand(0..MAX_HUE)
+end
+=end
 
 
